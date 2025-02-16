550c931c91fb3fb0bd5e202842c8948f5a4511e6
Akshay Gale <90633510+Akshaygale@users.noreply.github.com>
2025-02-16 16:56:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "ami_id" {
+        type = string
+}
+
+variable "aws_instance_name" {
+
+        type = string
+}
+
+variable "instance_type" {
+
+        type = string
+}
+
+
+variable "aws_bucket_name"{
+
+        type = string
+}
+
+variable "table_name" {
+        type = string
+}
+
+variable "env_name" {
+
+        type= string
+}
